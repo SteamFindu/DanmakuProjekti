1f85983048a109d36a66ae558a9d42b581010002
SteamFindu <arttu.hiekkanen@hotmail.com>
2020-05-05 13:47:45 +0300

    Initial project files

---
+++ Projektityo/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        
+        transform.Translate(Vector3.right * (horizontalInput * speed * Time.deltaTime));
+        transform.Translate(Vector3.up * (verticalInput * speed * Time.deltaTime));
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Unity danmaku game for fun
